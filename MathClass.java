257443a1de0d821a67b12b7f5fa55478680a1e74
ruteshr <44744748+ruteshr@users.noreply.github.com>
2021-04-15 09:08:14 +0530

    Create MathClass.java

---
+++ MathClass.java
@@ -0,0 +1,24 @@
+
+public class Mathclass {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Absolute: "+Math.abs(-4.7));
+		System.out.println("Minimum: "+Math.min(5,10));
+		System.out.println("Maximum: "+Math.max(5,10));
+		System.out.println("Square root: "+Math.min(5,10));
+		System.out.println("Random no form 0 to 1: "+Math.random());
+		System.out.println("Random no at specific Interval (0 to 100): "+(int)(Math.random()*101));
+
+	}
+
+}
+/*
+**OUTPUT**
+Absolute: 4.7
+Minimum: 5
+Maximum: 10
+Square root: 5
+Random no form 0 to 1: 0.6562353063052773
+Random no at specific Interval (0 to 100): 38
+*/
